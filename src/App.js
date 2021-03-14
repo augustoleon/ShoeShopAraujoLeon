346fd019f740cfc9287fd434c001872a2ffaf35c
Your Name <csr_4064@hotmail.com>
2021-03-13 21:23:22 -0300

    Desafio 2, Menú e-commerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          ShoeShop
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/augustoleon/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          gitHub
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
