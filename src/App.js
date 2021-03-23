9e69df9fd35e0e711886951cdaef17d3b1e3db5f
Your Name <csr_4064@hotmail.com>
2021-03-22 21:38:06 -0300

    agregado el greeting de "Crea tu langing"

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ItemListContainer/>
+      <ItemListContainer greeting= 'Component List here'/>
     </div>
   );
 }
--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -3,11 +3,11 @@ import {ItemCount} from '../ItemCount';
 
 const limitStock= 5;
 
-export const ItemListContainer= () =>{
+export const ItemListContainer= (props) =>{
 
     const onAdd = (number) => number === 0 ? alert(`No puedes agregar ${number} unidades al carrito`) : alert(`Agregaste ${number} unidades al carrito`)
 
-    return <div>Component List here
+    return <div>{props.greeting}
         <div class="container-fluid">
         <ItemCount stock={limitStock} initial={1} onAdd= {onAdd} />
 
